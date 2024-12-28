617efc59660f9cf161bdb2e7bbab586f1d8bfaa4
Aleksandar Tepavcevic <atepavcevic29@gmail.com>
2024-12-29 00:34:56 +0100

    setup nativewind

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -9,7 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="font-bold text-lg my-10">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
