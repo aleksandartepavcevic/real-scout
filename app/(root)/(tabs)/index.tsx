e39d1a87a16f574e5ec272b4ee1312738b38abf8
Aleksandar Tepavcevic <atepavcevic29@gmail.com>
2025-01-13 17:02:28 +0100

    feat: filters, cartds, search

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,27 +1,63 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-rubik text-xl my-10">
-        Edit app/index.tsx to edit this screen.
-      </Text>
-      <Link href="/explore">
-        <Text className="font-rubik text-xl my-10">Explore</Text>
-      </Link>
-      <Link href="/profile">
-        <Text className="font-rubik text-xl my-10">Profile</Text>
-      </Link>
-      <Link href="/sign-in">
-        <Text className="font-rubik text-xl my-10">Sign In</Text>
-      </Link>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                Adrian
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+        <Search />
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
+        </View>
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">
+            Our Recommendation
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <Filters />
+        <View className="flex flex-row gap-5 mt-5">
+          <Card />
+          <Card />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
